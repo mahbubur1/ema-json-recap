c0c3eed469ce25f56f239fe9c4861659b09765c0
mahbubur1 <mdmahbuborr462@gail.com>
2022-04-03 02:04:42 +0600

    event handler added to button

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,11 +3,19 @@ import './Product.css'
 
 const Product = (props) => {
     const {name,img,seller,price,ratings} = props.product;
+    const {handleAddToCart} =props;
     return (
         <div className='product'>
           <img src={img} alt=''></img>
-           <p className='product-name'>{name}</p>
-
+          <div className='product-info'>
+          <p className='product-name'>{name}</p>
+           <p>Price:$ {price}</p>
+           <p><small>Seller: {seller}</small></p>
+           <p><small>Ratings: {ratings}</small></p>
+          </div>
+          <button onClick={handleAddToCart} className='button-cart'>
+              <p>Add to cart</p>
+          </button>
         </div>
     );
 };
